859eb5ef9795713bd9cccff0355b627c8a7b1b24
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-01-07 21:08:35 +0300

    bot

---
+++ AuxProg.py
@@ -0,0 +1,6 @@
+# Мой проект AuxProg
+
+from modules.Main import Main
+
+if __name__ == '__main__':
+    main = Main()
---
+++ modules/Main.py
@@ -0,0 +1,15 @@
+import pyautogui as a
+
+class Main(object):
+    print(a.position()) #  Позиция курсора
+    print(a.size()) #  Размер экрана
+    a.PAUSE = 0.3
+    a.move(-200, 300) #  Переход в точку
+    a.move(-200, 300, duration = 0.5)  #  Перемещение курсора
+    a.moveTo(500, 500)  # Переход в точку
+    a.moveTo(100, 100, duration = 0.5)  # Переход в точку
+    a.click() #Клик
+    a.moveTo(800, 1080)
+    a.click(button='right')  # Клик
+    a.moveTo(850, 980)
+    a.click() #Клик
