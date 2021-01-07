8c8414be072f25efae860acc1a346434d01c142b
Nikita-2007 <nikita2007ua@gmail.com>
2021-01-07 21:08:21 +0300

    1 шаг

---
+++ AuxpPog.py
@@ -0,0 +1,4 @@
+from modules.Main import Main
+
+if __name__ == '__main__':
+    main = Main()
--- main.py
+++
@@ -1,7 +0,0 @@
-# Мой проект AuxProg
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
